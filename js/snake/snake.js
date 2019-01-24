eb647c2e70ec6bd9a65d4ad0805bd889d4a01d63
ljzzkkkss <793958447@qq.com>
2019-01-24 12:18:34 +0800

    crash algorithm

--- js/snake/snake.js
+++ js/snake/snake.js
@@ -21,6 +21,8 @@ export default class Snake {
     this.snakeArray = snakeArray; //整蛇数组
     //初始运动方向
     this.direction = 'right';
+    this.crashDirection = [];//碰撞方向可能有多个
+    this.crashed = false;
   }
 
   draw(ctx) {
@@ -29,16 +31,28 @@ export default class Snake {
     }
   }
 
-  move(food,timer,ctx) {
+  move(food, timer, ctx) {
+    //检测是否碰撞，如果已经到边缘并且当前移动方向和到达边缘时候的方向相同则会碰撞
+    if (this.crashed) {
+      if (this.crashDirection.indexOf(this.direction) != -1) {
+        //绘制当前碰撞状态，因为外面清除了画布
+        this.draw(ctx);
+        clearInterval(timer);
+        return;
+      } else {//改变了行动方向清除碰撞状态
+        this.crashDirection = [];
+        this.crashed = false;
+      }
+    }
+
     //此处是核心部分，蛇的 移动方式
     //1、画一个灰色的方块，位置与蛇头重叠
     //2、将这个方块插到数组中蛇头后面一个的位置
     //3、砍去末尾的方块
     //4、将蛇头向设定方向移动一格
-    var rect = new Rect(this.head.x, this.head.y, this.head.w, this.head.h, "gray");
+    var rect = new Rect(this.head.x, this.head.y,    this.head.w, this.head.h, "gray");
 
     this.snakeArray.splice(1, 0, rect);
-    
     
 
     switch (this.direction) {
@@ -70,13 +84,36 @@ export default class Snake {
     this.draw(ctx);
     // gameover判定
     // 撞墙
-    if (this.head.x + 20 >= canvas.width || this.head.x < 0 || this.head.y + 20 >= canvas.height || this.head.y < 0) {
-      clearInterval(timer);
+    if (this.head.x + 20 >= canvas.width) {
+      if (this.crashDirection.indexOf('right') == -1) {
+        this.crashDirection.push('right');
+      }
+      this.crashed = true;
+    }
+    if (this.head.x == 0) {
+      if (this.crashDirection.indexOf('left') == -1) {
+        this.crashDirection.push('left');
+      }
+      this.crashed = true;
+    }
+    if (this.head.y + 20 >= canvas.height) {
+      if (this.crashDirection.indexOf('down') == -1) {
+        this.crashDirection.push('down');
+      }
+      this.crashed = true;
+    }
+    if (this.head.y == 0) {
+      if (this.crashDirection.indexOf('up') == -1) {
+        this.crashDirection.push('up');
+      }
+      this.crashed = true;
     }
     // 撞自己，循环从1开始，避开蛇头与蛇头比较的情况
+    //TODO:此处有误待修改,参见撞墙判定
     for (var i = 1; i < this.snakeArray.length; i++) {
       if (this.snakeArray[i].x == this.head.x && this.snakeArray[i].y == this.head.y) {
-        clearInterval(timer);
+        this.crashDirection.push(this.direction);
+        this.crashed = true;
       }
     };
   }
